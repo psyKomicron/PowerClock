e46cd39088243dc747ea242c5eaad045641ca02c
psykomicron <psykomicron@protonmail.com>
2022-06-28 22:44:07 +0200

    added new headers

--- PowerClock/PowerClock/PowerClock/pch.h
+++ PowerClock/PowerClock/PowerClock/pch.h
@@ -9,15 +9,19 @@
 #undef GetCurrentTime
 
 #include <microsoft.ui.xaml.window.h>
+#include <dispatcherqueue.h>
 
 #include <winrt/Microsoft.UI.Composition.h>
+#include <winrt/Microsoft.UI.Composition.SystemBackdrops.h>
 #include <winrt/Microsoft.UI.Dispatching.h>
 #include <winrt/Microsoft.UI.Interop.h>
+#include <winrt/Microsoft.UI.Input.h>
 #include <winrt/Microsoft.UI.Windowing.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Microsoft.UI.Xaml.Data.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
 #include <winrt/Microsoft.UI.Xaml.Interop.h>
 #include <winrt/Microsoft.UI.Xaml.Markup.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
@@ -25,6 +29,7 @@
 #include <winrt/Microsoft.UI.Xaml.Shapes.h>
 #include <winrt/Microsoft.Windows.AppLifecycle.h>
 #include <winrt/Microsoft.Windows.ApplicationModel.Resources.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.Data.Xml.Dom.h>
@@ -35,8 +40,6 @@
 #include <winrt/Windows.UI.Notifications.h>
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Media.Animation.h>
-
-#include <wil/cppwinrt_helpers.h>
 
 
 #pragma comment(lib, "PowrProf.lib")
